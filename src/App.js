10ec7456706f51370806ab2931d460dc969822eb
Ian Gardner <ian.gardner@siemens.com>
2023-08-05 09:57:01 +0100

    Testing static web app database stuff

--- src/App.js
+++ src/App.js
@@ -11,8 +11,21 @@ function App() {
   });
 
   return <div>
-    <div>Text from front end!</div>    
+    <div>Text from front end!</div>
     <div>{data}</div>
+    <div>
+      <h1>Static Web Apps Database Connections</h1>
+      <blockquote>
+        Open the console in the browser developer tools to see the API responses.
+      </blockquote>
+      <div>
+        <button id="list" onclick="list()">List</button>
+        <button id="get" onclick="get()">Get</button>
+        <button id="update" onclick="update()">Update</button>
+        <button id="create" onclick="create()">Create</button>
+        <button id="delete" onclick="del()">Delete</button>
+      </div>
+    </div>
   </div>;
 }
 
